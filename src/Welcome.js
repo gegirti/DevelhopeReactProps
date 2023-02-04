52d204f202a20ca089c8878cbb24e8761636c7e4
Taha <atgulderen@gmail.com>
2023-02-05 00:11:02 +0300

    second prop, age, within a p tag

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,8 +3,14 @@ import React from "react";
 export class Welcome extends React.Component {
   static defaultProps = {
     name: "fellow human being",
+    age: 1,
   };
   render() {
-    return <p>Welcome {this.props.name}!</p>;
+    return (
+      <div>
+        <p>Welcome {this.props.name}!</p>
+        <p>Your age is {this.props.age}</p>
+      </div>
+    );
   }
 }
