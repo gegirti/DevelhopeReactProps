fabcc10d579c8aa1e9df57e51bc8705dddf9bb68
Taha <atgulderen@gmail.com>
2023-02-05 00:06:33 +0300

    default name value for Welcome class

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 export class Welcome extends React.Component {
+  static defaultProps = {
+    name: "fellow human being",
+  };
   render() {
     return <p>Welcome {this.props.name}!</p>;
   }
