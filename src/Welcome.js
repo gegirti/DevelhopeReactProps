f401c6de24650a5f0797f0fccf229320e280dfe3
Taha <atgulderen@gmail.com>
2023-02-05 00:35:40 +0300

    Age extracted

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+class Age extends React.Component {
+  render() {
+    return <p>Your age is {this.props.age}</p>;
+  }
+}
+
 export class Welcome extends React.Component {
   static defaultProps = {
     name: "fellow human being",
@@ -9,7 +15,7 @@ export class Welcome extends React.Component {
     return (
       <div>
         <p>Welcome {this.props.name}!</p>
-        <p>Your age is {this.props.age}</p>
+        <Age age="14" />
       </div>
     );
   }
